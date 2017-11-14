159de16a5642481d7f73b3f73b41046bfdad6a93
sefialbo <sefialbo@gmail.com>
2017-11-14 23:42:14 +0200

    add vm

---
+++ findResources.ps1
@@ -0,0 +1,13 @@
+﻿Login-AzureRmAccount
+$subscriptions = Get-AzureRmSubscription
+foreach ($sub in $subscriptions) {
+  Set-AzureRmContext -SubscriptionId $sub.SubscriptionId -TenantId $sub.TenantId
+  $resources = Get-AzureRmResource
+  foreach ($resource in $resources) {
+     If ($resource.ResourceType -eq "Microsoft.Storage/storageAccounts") {
+         $y = Invoke-AzureRmResourceAction -Action listKeys -ResourceType "Microsoft.Storage/storageAccounts" `
+         -ResourceGroupName $resource.ResourceGroupName -Name $resource.ResourceName
+         echo $y
+     }
+  }
+}
---
+++ runScriptOnVM.ps1
@@ -0,0 +1,19 @@
+﻿# Run script on VM
+# check these commands:
+$PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server"}'
+
+Set-AzureRmVMExtension -ExtensionName "IIS" -ResourceGroupName $resourceGroup -VMName $vmName `
+  -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion 1.4 `
+  -SettingString $PublicSettings -Location $location
+
+  # create vm object
+$vm = Get-AzureVM -Name 2016clas -ServiceName 2016clas1313
+
+# set extension
+Set-AzureRmVMCustomScriptExtension -ResourceGroupName "ResourceGroup11" `
+-Location "Central US" -VMName "VirtualMachine07" `
+-Name "ContosoTest" -TypeHandlerVersion "1.1" -StorageAccountName "Contoso" `
+-StorageAccountKey <StorageKey> -FileName "ContosoScript.exe" -ContainerName "Scripts"
+
+# update vm
+$vm | Update-AzureVM
